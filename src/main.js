a9521561bebf76866654ec8ac66b27cfcdc300fd
Pedro Henrique de França <francahenriquepedro@gmail.com>
2018-06-06 22:34:51 -0300

    webpack install & config & update

--- src/main.js
+++ src/main.js
@@ -1,3 +1,8 @@
-const a = 1
+import * as R from 'ramda';
 
-console.log(a);
+const arr1 = [1, 1, 1, 2, 2, 3, 4, 5, 6, 6];
+const arr2 = [5, 6, 6, 7, 7, 7, 8, 9, 10];
+
+const arr3 = R.union(arr1, arr2);
+
+console.log(arr3);
---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    filename: './src/main.js',
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'build.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: [
+            ['es2015', { modules: false }],
+          ],
+        },
+      },
+    ],
+  },
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin({
+        cache: true,
+        parallel: true,
+        uglifyOptions: {
+          compress: false,
+          ecma: 6,
+          mangle: true,
+        },
+        sourceMap: true,
+      }),
+    ],
+  },
+};
